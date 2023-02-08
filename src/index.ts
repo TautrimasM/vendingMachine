3357c572f823561355b385bbfd052934fe14bf3c
TautrimasM <115977446+TautrimasM@users.noreply.github.com>
2023-02-08 12:08:23 +0200

    Update index.ts
    creaned up code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { vendingMachine } from "./vendingMachine";
 
-// var firstName = process.argv[2]; // Will be set to 'Sean'
-// var lastName = process.argv[3]; // Will be set to 'Worthington'
-//console.log(process.argv.length);
 let coinsInserted = [];
-// = [
-//   { num: 3, val: 20 },
-//   { num: 5, val: 50 },
-// ];
+
 const priceInCents = Number(process.argv[2]);
 for (let i = 3; i < process.argv.length; i++) {
   coinsInserted.push({ num: 1, val: Number(process.argv[i]) });
